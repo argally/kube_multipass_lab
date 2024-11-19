ef93615d3c1147f688ffb717622fb1adeddfd103
argally <amandargalligan@gmail.com>
2024-11-19 12:02:31 +0000

    fix: enable two workers nodes

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 module "multipass_kube_lab" {
-  source = "git@github.com:argally/terraform-kubernetes-multipass.git"
+  source  = "git@github.com:argally/terraform-kubernetes-multipass.git"
+  workers = 2
 }
 
 
 module "kube_addons" {
-  source = "git@github.com:argally/terrraform-modules-kube.git"
+  source = "../terraform-modules-kube/"
 
-  enable_kube_addons          = true
+  enable_kube_addons          = false
   argocd_admin_password       = var.argocd_admin_password
   argocd_api_user_password    = var.argocd_api_user_password
   argocd_github_repo_username = var.argocd_github_repo_username
